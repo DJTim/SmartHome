1b7a45cacacb6a0c9c34311e6f0508bcecfe4926
DJTim <tim@Tims-MacBook-Pro.local>
2015-01-03 18:44:14 +0100

    mongoDB preparations

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+import json
 import bottle
 import subprocess
 from bottle import jinja2_template as template, static_file, request, app
 from bottle import redirect
+from pymongo import Connection
+
+connection = Connection('localhost', 27017)
+db = connection.domotica
 
 @bottle.route('/')
 def index():
@@ -30,5 +35,25 @@ def kaku_off(id):
 @bottle.route('/<id>/<dim>')
 def kaku_off(id, dim):
     subprocess.Popen(["sudo", "./kaku/kaku", "8631674", id, dim])
+    
+@bottle.route('/api', method='PUT')
+def put_document():
+    data = request.body.readline()
+    if not data:
+        abort(400, 'No data received')
+    entity = json.loads(data)
+    if not entity.has_key('_id'):
+        abort(400, 'No _id specified')
+    try:
+        db['rooms'].save(entity)
+    except ValidationError as ve:
+        abort(400, str(ve))
+     
+@bottle.route('/api/:id', method='GET')
+def get_document(id):
+    entity = db['rooms'].find_one({'_id':id})
+    if not entity:
+        abort(404, 'No document with id %s' % id)
+    return entity
 
 bottle.run(host='192.168.2.215', port=8080, debug=False)
